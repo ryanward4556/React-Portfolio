9d944652a4872621cb37d25eabe4908485bf7769
Ryan Ward <ryanward4556@gmail.com>
2019-12-10 13:26:22 -0500

    heroku 2

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import "animate.css/animate.min.css";
 import Landing from "../Landing";
-import About from "../About";
+// import About from "../About";
 import Portfolio from "../Portfolio";
 
 const App = () => {
   return (
         <Switch>
           <Route exact path="/" component={Landing} />
-          <Route exact path="/about" component={About} />
+          {/* <Route exact path="/about" component={About} /> */}
           <Route exact path="/portfolio" component={Portfolio} />
         </Switch>
   );
