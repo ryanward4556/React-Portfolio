d0a57579cc8c64426922c132af5abfa00122fba4
Ryan Ward <ryanward4556@gmail.com>
2019-12-10 13:54:36 -0500

    removed NavBar and switched to only darkmodetoggle

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import "./style.css";
-import NavBar from "../NavBar";
+// import "./style.css";
+// import NavBar from "../NavBar";
+import DarkModeToggle from "../DarkModeToggle";
 import Footer from "../Footer";
 import { Zoom } from 'react-reveal';
 import ProjectCard from "../ProjectCard";
@@ -9,17 +10,20 @@ import ProjectCard from "../ProjectCard";
 function Portfolio() {
     return (
         <>
-        <NavBar></NavBar>
-        <div className="content">
-            <div>
-                <Zoom>
-                    <h1>Projects</h1>
-                </Zoom>
+            {/* <NavBar></NavBar> */}
+            <nav className="navbar navbar-expand-lg justify-content-end my-navbar fixed-top">
+                <DarkModeToggle></DarkModeToggle>
+            </nav>
+            <div className="content">
+                <div>
+                    <Zoom>
+                        <h1>Projects</h1>
+                    </Zoom>
+                </div>
+                <ProjectCard></ProjectCard>
+                <Footer></Footer>
             </div>
-            <ProjectCard></ProjectCard>
-            <Footer></Footer>
-        </div>
-    </>
+        </>
     )
 }
 
